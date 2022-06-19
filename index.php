ad3d73a2dd24080f860ceb6879136f596cb8971e
Oleg.Laptev <olegpal55@gmail.com>
2022-06-19 13:27:43 +0300

    74

--- index.php
+++ index.php
@@ -2,22 +2,14 @@
 	require_once 'Form.php';
 	require_once 'Input.php';
 	require_once 'Submit.php';
+	require_once 'pas.php';
 
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 
-	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 	echo $form->open();
-		echo (new Input())->setAttrs(['name'=>'num1', 'value' => '1']);
-		echo (new Input())->setAttrs(['name'=>'num2', 'value' => '2']);
-		echo (new Input())->setAttrs(['name'=>'num3', 'value' => '3']);
-		echo (new Input())->setAttrs(['name'=>'num4', 'value' => '4']);
-		echo (new Input())->setAttrs(['name'=>'num5', 'value' => '5']);
-		echo (new Input())->setAttrs(['type'=>'submit']);
 		echo (new Input)->setAttr('name', 'year');
+		echo (new Input)   ->setAttr('name', 'login');
+		echo (new pas)->setAttr('name', 'password');
 		echo new Submit;
 	echo $form->close();
-
-	if(isset($_GET['num1'])){
-	echo $sum = $_GET['num1'] + $_GET['num2'] + $_GET['num3'] + $_GET['num4'] + $_GET['num5'];
-	}
 ?>
