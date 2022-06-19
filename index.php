1ec4192e55eb3aa72b72dcca78c5cc36c18300b0
Oleg.Laptev <olegpal55@gmail.com>
2022-06-19 13:26:58 +0300

    73

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
 	require_once 'Form.php';
 	require_once 'Input.php';
+	require_once 'Submit.php';
 
-	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'POST']);
+	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 
 	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 	echo $form->open();
-
 		echo (new Input())->setAttrs(['name'=>'num1', 'value' => '1']);
 		echo (new Input())->setAttrs(['name'=>'num2', 'value' => '2']);
 		echo (new Input())->setAttrs(['name'=>'num3', 'value' => '3']);
 		echo (new Input())->setAttrs(['name'=>'num4', 'value' => '4']);
 		echo (new Input())->setAttrs(['name'=>'num5', 'value' => '5']);
 		echo (new Input())->setAttrs(['type'=>'submit']);
+		echo (new Input)->setAttr('name', 'year');
+		echo new Submit;
 	echo $form->close();
 
 	if(isset($_GET['num1'])){
