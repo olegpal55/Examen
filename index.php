709fdbbe355a003296b01a9d37f4c81fda6bf277
Oleg.Laptev <olegpal55@gmail.com>
2022-06-19 13:28:16 +0300

    75

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 	require_once 'Input.php';
 	require_once 'Submit.php';
 	require_once 'pas.php';
+	require_once 'Hidden.php';
 
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 
 	echo $form->open();
-		echo (new Input)->setAttr('name', 'year');
 		echo (new Input)   ->setAttr('name', 'login');
 		echo (new pas)->setAttr('name', 'password');
+		echo (new Hidden)->setAttr('name', 'id')->setAttr('value', '123');
+		echo (new Input)->setAttr('name', 'year');
 		echo new Submit;
 	echo $form->close();
 ?>
