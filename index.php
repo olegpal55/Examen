7a377363d39a8ee55ae9dc2577f03507c26d423d
Oleg.Laptev <olegpal55@gmail.com>
2022-06-19 13:24:47 +0300

    71

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 	require_once 'HtmlList.php';
-	$list = new HtmlList('ul');
 	require_once 'ClassUL.php';
 	require_once 'ClassOl.php';
 	$ul = new Ul;
@@ -11,14 +10,17 @@
 		->addItem((new ListItem())->setText('item2'))
 		->addItem((new ListItem())->setText('item3'))
 		->show();
+	require_once 'Form.php';
 
-	echo $list
 	echo '<br>';
 	echo $ul
 		->addItem((new ListItem())->setText('item1'))
 		->addItem((new ListItem())->setText('item2'))
 		->addItem((new ListItem())->setText('item3'))
 		->show();
-		->show();
+	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'POST']);
 
+	echo $form->open();
+
+	echo $form->close();
 ?>
