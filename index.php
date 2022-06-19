4321611a17ff03599ea1f8f1ed40ae4601db479b
Oleg.Laptev <olegpal55@gmail.com>
2022-06-19 11:37:26 +0300

    58

--- index.php
+++ index.php
@@ -1,36 +1,26 @@
 <?php
 require_once 'date.php';
-    $date = new Date('2025-12-31');
-
-    echo $date->getYear(); echo ' ';  // выведет '2025'
-    echo $date->getMonth(); echo ' '; // выведет '12'
-    echo $date->getDay(); echo ' '; // выведет '31' 
-
-    echo '<br>';
-
-    echo $date->getWeekDay(); echo ' ';    // выведет '3'
-    echo $date->getWeekDay('ru'); echo ' '; // выведет 'среда'
-    echo $date->getWeekDay('en'); echo ' '; // выведет 'wednesday'
-
-    echo '<br>';
-
-    echo (new Date('2025-12-31'))->addYear(1); // '2026-12-31'
-    echo '<br>';
-    echo (new Date('2025-12-31'))->addDay(1);  // '2026-01-01'
-    echo '<br>';
-    echo (new Date('2025-12-31'))->subDay(3); // '2025-12-28'
-    echo '<br>';
-    echo (new Date('2025-12-28'))->addYear(1); // '2026-12-28'
-?> 
 require_once 'interval.php';
+require_once 'iFile.php';
+require_once 'File.php';
 
-    $date1 = new Date('2025-12-31');
-    $date2 = new Date('2026-11-28');
+	$date1 = new Date('2025-12-31');
+	$date2 = new Date('2026-11-28');
+	$file = new File('index.php');
 
-    $interval = new Interval($date1, $date2);
+echo $file->getPath().'<br>';
+echo $file->getDir().'<br>';
+echo $file->getExt().'<br>';
+echo $file->getName().'<br>';
+echo $file->getSize().' байт'.'<br>';
 
-    echo $interval->toDays();   // выведет разницу в днях
-    echo $interval->toMonths(); // выведет разницу в месяцах
-    echo $interval->toYears();  // выведет разницу в годах
+echo $file->getText();
 
-?>
+	$interval = new Interval($date1, $date2);
+
+	echo $interval->toDays();   // выведет разницу в днях
+	echo '<br>';
+	echo $interval->toMonths(); // выведет разницу в месяцах
+	echo '<br>';
+	echo $interval->toYears();  // выведет разницу в годах
+?>zxa
