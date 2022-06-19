251a989d7b13446bacd14d72246504adc3f3ed2b
Oleg.Laptev <olegpal55@gmail.com>
2022-06-19 13:26:01 +0300

    72

--- index.php
+++ index.php
@@ -1,26 +1,21 @@
 <?php
-	require_once 'HtmlList.php';
-	require_once 'ClassUL.php';
-	require_once 'ClassOl.php';
-	$ul = new Ul;
-	$ol = new Ol;
-
-	echo $ol
-		->addItem((new ListItem())->setText('item1'))
-		->addItem((new ListItem())->setText('item2'))
-		->addItem((new ListItem())->setText('item3'))
-		->show();
 	require_once 'Form.php';
+	require_once 'Input.php';
 
-	echo '<br>';
-	echo $ul
-		->addItem((new ListItem())->setText('item1'))
-		->addItem((new ListItem())->setText('item2'))
-		->addItem((new ListItem())->setText('item3'))
-		->show();
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'POST']);
 
+	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 	echo $form->open();
 
+		echo (new Input())->setAttrs(['name'=>'num1', 'value' => '1']);
+		echo (new Input())->setAttrs(['name'=>'num2', 'value' => '2']);
+		echo (new Input())->setAttrs(['name'=>'num3', 'value' => '3']);
+		echo (new Input())->setAttrs(['name'=>'num4', 'value' => '4']);
+		echo (new Input())->setAttrs(['name'=>'num5', 'value' => '5']);
+		echo (new Input())->setAttrs(['type'=>'submit']);
 	echo $form->close();
+
+	if(isset($_GET['num1'])){
+	echo $sum = $_GET['num1'] + $_GET['num2'] + $_GET['num3'] + $_GET['num4'] + $_GET['num5'];
+	}
 ?>
