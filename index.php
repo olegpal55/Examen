3ccf5b28f3a86c01a89e97819538040845e7986f
Oleg.Laptev <olegpal55@gmail.com>
2022-06-19 13:29:12 +0300

    76

--- index.php
+++ index.php
@@ -4,14 +4,15 @@
 	require_once 'Submit.php';
 	require_once 'pas.php';
 	require_once 'Hidden.php';
+	require_once 'Textarea.php';
 
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 
 	echo $form->open();
-		echo (new Input)   ->setAttr('name', 'login');
-		echo (new pas)->setAttr('name', 'password');
 		echo (new Hidden)->setAttr('name', 'id')->setAttr('value', '123');
 		echo (new Input)->setAttr('name', 'year');
+		echo (new Input)->setAttr('name', 'user');
+		echo (new Textarea)->setAttr('name', 'message')->show();
 		echo new Submit;
 	echo $form->close();
 ?>
