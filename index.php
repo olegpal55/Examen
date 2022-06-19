f27f9f14243f1bb046840f6c8f1f1c64d95493bb
Oleg.Laptev <olegpal55@gmail.com>
2022-06-19 13:29:57 +0300

    77

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
 	require_once 'pas.php';
 	require_once 'Hidden.php';
 	require_once 'Textarea.php';
+	require_once 'Checkbox.php';
 
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
+	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 
 	echo $form->open();
-		echo (new Hidden)->setAttr('name', 'id')->setAttr('value', '123');
-		echo (new Input)->setAttr('name', 'year');
+		echo (new Checkbox)->setAttr('name', 'checkbox');
 		echo (new Input)->setAttr('name', 'user');
 		echo (new Textarea)->setAttr('name', 'message')->show();
 		echo new Submit;
